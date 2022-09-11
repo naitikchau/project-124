18610ea3437c512225509732b76a44e8de6b1a9a
naitikchau <104361541+naitikchau@users.noreply.github.com>
2022-09-11 15:04:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function setup()
 {
     video = createCapture(VIDEO);
-    video.size(550, 500);
+    video.size(450, 400);
 
-    canvas = createCanvas(550, 500);
+    canvas = createCanvas(450, 400);
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
